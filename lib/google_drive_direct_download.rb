cb31572b5fed3930f84afbd331a2816b3b9dc9d6
Ítalo Matos <italomatos@gmail.com>
2022-02-12 18:19:38 -0300

    add google direct download (#1)

--- lib/google_drive_direct_download.rb
+++ lib/google_drive_direct_download.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "google_drive_direct_download/version"
-require_relative "google_drive_direct_download/google/docs/direct_download"
-require_relative "google_drive_direct_download/google/drive/direct_download"
+require_relative 'google_drive_direct_download/version'
+require_relative 'google_drive_direct_download/google/google_direct_download'
+require_relative 'google_drive_direct_download/google/docs/direct_download'
+require_relative 'google_drive_direct_download/google/drive/direct_download'
 
 module GoogleDriveDirectDownload
   class Error < StandardError; end
